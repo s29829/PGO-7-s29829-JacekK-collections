428ba3373501888000d6e743a793a30487df9911
jacekkowalczyk <s29829@pjwstk.edu.pl>
2024-06-18 10:06:38 +0200

    v001

--- src/Item.java
+++ src/Item.java
@@ -1,6 +1,46 @@
 public class Item {
 
+    private String name;
+    private String code;
+    private int codeNumber;
 
+    public Item(String code, String name) {
+        setName(name);
+        setCode(code);
 
-    public void show (){}
+    }
+
+    public Item(int codeNumber, String name) {
+        this.name = name;
+        this.codeNumber = codeNumber;
+    }
+
+    public int getCodeNumber() {
+        return codeNumber;
+    }
+
+    public void setCodeNumber(int codeNumber) {
+        this.codeNumber = codeNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void show() {
+        System.out.println("Name: " + name);
+//        System.out.println("Code: " + code);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,43 @@
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
 
+//        Zadanie 1.
+        ArrayList<Car> car = new ArrayList<>();
 
-        ArrayList <Car> car = new ArrayList<>();
+//        Zadanie 2.
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item("Spoon steel", "10345"));
+        items.add(new Item("Spoon silver", "10256"));
+        items.add(new Item("Plate white", "20056"));
+        items.add(new Item("Big plate white", "20077"));
+        items.add(new Item("Pho soup", "51123"));
+        items.add(new Item("Chicken with rice", "40087"));
 
-        ArrayList <Item> items = new ArrayList<>();
+        for (Item item : items) {
+            item.show();
+        }
+        ArrayList<Object> itemsHasMap = new ArrayList<>();
+        HashMap<String, String> objectObjectHashMap = new HashMap<>();
 
+        for (Item item : items) {
+            objectObjectHashMap.put(item.getCode(), item.getName());
+        }
+
+        System.out.println(objectObjectHashMap.entrySet());
+
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(new Item("","Spoon steel"));
+        items.add(new Item("","Spoon silver"));
+        items.add(new Item("","Plate white"));
+        items.add(new Item("","Big plate white"));
+        items.add(new Item("","Pho soup"));
+        items.add(new Item("","Chicken with rice"));
+//        Zadanie 3.
 
 //        Map<Integer, String> employees = new HashMap<>();
 //        employees.put(111, "Jan Kowalski");
